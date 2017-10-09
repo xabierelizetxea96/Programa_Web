c3ad00b41ae82b8acd00e8de6515d4f085d35eeb
xabierelizetxea96 <leizaran_xabi@hotmail.com>
2017-10-09 12:29:54 +0200

    Initial commit

---
+++ app/src/main/java/elizetxea/xabier/web/Web.java
@@ -0,0 +1,13 @@
+package elizetxea.xabier.web;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Web extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+    }
+}
